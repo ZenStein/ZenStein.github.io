d9901b1b7d20b8bb91715ca84fd7d0bf25f23ea9
ZenStein <shaneenterprises@gmail.com>
2015-11-13 21:32:55 -0800

    replace comments hashes

--- phase-0/week-4/variable-methods.rb
+++ phase-0/week-4/variable-methods.rb
@@ -28,19 +28,20 @@ puts suggest_bigger_number
 
 
 
-
-#How do you define a local variable?#
+=begiin
+How do you define a local variable?
  using the = which is the assignment operator
 How do you define a method?
 def SOME_METHOD SOMEPARAM1, SOMEPARAM2
-  \#code here.....
+  //code here.....
 end
-#What is the difference between a local variable and a method?#
+What is the difference between a local variable and a method?
 a local variable refers to value that is stored in memory, a method os a function
 that is mainly used for applying operations to variables
-#How do you run a ruby program from the command line?#
+How do you run a ruby program from the command line?
 ruby some_ruby_file.rb
-#How do you run an RSpec file from the command line?#
+How do you run an RSpec file from the command line?
 rspec some_rspec_file.rb
-#What was confusing about this material? What made sense?#
+What was confusing about this material? What made sense?
 all of it made sens, very basic, simple.
+=end
