64f01afcccd393d4ffd96d102daf8b807e99983b
ZenStein <shaneenterprises@gmail.com>
2015-11-16 02:23:39 -0800

    add blog arrays-hashes

---
+++ phase-0/week-4/add-it-up/saver.rb
@@ -0,0 +1,33 @@
+Links:
+
+
+
+factorial / add it up
+
+
+What are methods? Why are they useful?
+methods are function that can be called, which run 'snippets' of code that can be passed variable values.  This is usefull for various reasons, some of which are, keeping code organized, clean and concise.  As well as being able to re use code for redundant evaluations and tasks.
+What are local variables? Where are they able to be accessed (HINT: try accessing a local variable outside of a method to see).
+local variables are variables that are available to you in your current scope. They are able to be accessed while in the scope of when they were defined, and although they cannot be accessed within a method, their value can be referenced and passed in as an argument to a method.  As long as that variable is available to you in the scope.
+What is a method's argument? How would you use that?
+a methods argument is the value that you passed to it's parameter.  i.e. for function times_two(5), you would say the argument here is 5, which is passed into some code that would probably look something like
+
+def times_two SOME_PARAM
+
+SOME_PARAM * 2
+
+end
+
+where SOME_PARAM would be the parameter.
+
+How does Ruby know what to return in a method?
+  Yes, the last line in a method will be returned
+
+What does it mean to iterate (or loop) over arrays or hashes?
+to iterate or loop over arrays or hashes means to to 'visit' each key, value and run some type of code on said value.
+
+What new Ruby methods did you learn about in this challenge?
+To make sure to look over the docs, and not assume that the language is similar to other languages.  Even though there are similarities, one wrong assumption can be costly, as it's common to  over look the things you 'know' or are at least sure of, while debugging.
+
+What did you learn from your pair in this challenge?
+To make sure to look over the docs, and when your basic initial code isn't working, going back to your pseudo code might be a good idea.
